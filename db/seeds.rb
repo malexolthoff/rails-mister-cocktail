355b7cb73bb6e9ee36c55cdeb3e909649a5c28b3
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-02 15:47:46 -0400

    Sorted Cocktails Controller, made a simple submission form, sorted routes, and made a simple index page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+list = JSON.parse(ingredients_serialized)
+
+drinks = list.flatten.drop(1)
+n = 0
+drinks.each do |hash|
+  hash.each do |hash|
+    hash.each_value do |value|
+      Ingredient.create({name: value})
+    end
+  end
+end
+
